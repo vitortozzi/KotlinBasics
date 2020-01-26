6e4bc4de12a9556eb2477e23366d7c58604738d9
Vítor Tozzi <vitortozzi@gmail.com>
2020-01-26 15:29:32 -0300

    Initial project setup

---
+++ FinancasK/app/src/main/java/br/com/tozzilabs/financask/ui/activity/ListaTransacoesActivity.kt
@@ -0,0 +1,13 @@
+package br.com.tozzilabs.financask.ui.activity
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import br.com.tozzilabs.financask.R
+
+class ListaTransacoesActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_lista_transacoes)
+    }
+}
